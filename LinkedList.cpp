24897f9133e84b56bc695ed103380e02bc005e31
pratyaydeep <pratyaydeepghanta@gmail.com>
2021-11-15 10:17:23 +0530

    tab to space change

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,47 +5,47 @@ using namespace std;
 
 class linked_list{
 public:
-	ll data;
-	linked_list *next;
-	linked_list(){
-		this->next=NULL;
-	}
-	linked_list(ll data){
-		this->data=data;
-		this->next=NULL;
-	}
-	linked_list *add(ll data, linked_list *head){
-		if(head==NULL){
-			linked_list *temp=new linked_list(data);
-			head=temp;
-		}else{
-			linked_list *temp=head;
-			while(temp->next!=NULL)
-				temp=temp->next;
-			temp->next=new linked_list(data);
-		}
-		return head; 
-	}
+    ll data;
+    linked_list *next;
+    linked_list(){
+        this->next=NULL;
+    }
+    linked_list(ll data){
+        this->data=data;
+        this->next=NULL;
+    }
+    linked_list *add(ll data, linked_list *head){
+        if(head==NULL){
+            linked_list *temp=new linked_list(data);
+            head=temp;
+        }else{
+            linked_list *temp=head;
+            while(temp->next!=NULL)
+                temp=temp->next;
+            temp->next=new linked_list(data);
+        }
+        return head; 
+    }
 
-	void print(linked_list *head){
-		linked_list *temp=head;
-		while(temp!=NULL){
-			cout<<temp->data<<" ";
-			temp=temp->next;
-		}
-	}
+    void print(linked_list *head){
+        linked_list *temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
 };
 
 signed main(){
-	// TODO: start from slow pointers and fast pointers
+    // TODO: start from slow pointers and fast pointers
 
-	linked_list *head=NULL;
-	head=head->add(1,head);
-	head=head->add(2,head);
-	head=head->add(3,head);
-	head=head->add(4,head);
-	head=head->add(5,head);
+    linked_list *head=NULL;
+    head=head->add(1,head);
+    head=head->add(2,head);
+    head=head->add(3,head);
+    head=head->add(4,head);
+    head=head->add(5,head);
 
-	head->print(head);
-	return 0;
+    head->print(head);
+    return 0;
 }
